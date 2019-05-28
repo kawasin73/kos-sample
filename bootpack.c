80e64dc466aee45cb76789984508257d4223359b
Kawamura Shintaro <kawasin73@gmail.com>
2019-05-28 19:07:10 +0900

    Chapter 4 : Change pointer usage

--- bootpack.c
+++ bootpack.c
@@ -5,11 +5,10 @@ void io_hlt(void);
 void HariMain(void)
 {
     int i;
-    char *p;
+    char *p = 0xa0000;
 
-    for (i = 0xa0000; i <= 0xaffff; i++) {
-        p = (char *) i;
-        *p = i & 0x0f;
+    for (i = 0; i <= 0xffff; i++) {
+        *(p+i) = i & 0x0f;
     }
 
     for (;;) {
