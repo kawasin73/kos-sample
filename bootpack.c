c366a95b39488bcb8b7839d3183c1c35d9bfddf6
Kawamura Shintaro <kawasin73@gmail.com>
2019-05-31 21:19:51 +0900

    Chapter 5 : Show mouse cursor

--- bootpack.c
+++ bootpack.c
@@ -12,6 +12,8 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 void init_screen(char *vram, int xsize, int ysize);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
 
 #define COL8_000000     0
 #define COL8_FF0000     1
@@ -39,18 +41,24 @@ struct BOOTINFO {
 void HariMain(void)
 {
     struct BOOTINFO *binfo;
-    char s[40];
+    char s[40], mcursor[256];
+    int mx, my;
 
     init_palette();
 
     binfo = (struct BOOTINFO *) 0x0ff0;
 
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-    putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
-    sprintf(s, "scrnx = %d", binfo->scrnx);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
+    init_mouse_cursor8(mcursor, COL8_008484);
+
+    /* 画面中央になるように計算 */
+    mx = (binfo->scrnx - 16) / 2;
+    my = (binfo->scrny - 28 - 16) / 2;
+
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+    sprintf(s, "(%d, %d)", mx, my);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     for (;;) {
         io_hlt();
@@ -147,6 +155,56 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s
     for (; *s != 0x00; s++) {
         putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
         x += 8;
+    }
+    return;
+}
+
+/* マウスカーソルを準備 (16 x 16) */
+void init_mouse_cursor8(char *mouse, char bc) {
+    static char cursor[16][16] = {
+        "**************..",
+        "*OOOOOOOOOOO*...",
+        "*OOOOOOOOOO*....",
+        "*OOOOOOOOO*.....",
+        "*OOOOOOOO*......",
+        "*OOOOOOO*.......",
+        "*OOOOOOO*.......",
+        "*OOOOOOOO*......",
+        "*OOOO**OOO*.....",
+        "*OOO*..*OOO*....",
+        "*OO*....*OOO*...",
+        "*O*......*OOO*..",
+        "**........*OOO*.",
+        "*..........*OOO*",
+        "............*OO*",
+        ".............***"
+    };
+    int x, y;
+
+    for (y = 0; y < 16; y++) {
+        for (x = 0; x < 16; x++) {
+            switch (cursor[y][x]) {
+            case '*':
+                mouse[y*16+x] = COL8_000000;
+                break;
+            case 'O':
+                mouse[y*16+x] = COL8_FFFFFF;
+                break;
+            case '.':
+                mouse[y*16+x] = bc;
+                break;
+            }
+        }
+    }
+    return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize) {
+    int x, y;
+    for (y = 0; y < pysize; y++) {
+        for(x = 0; x < pxsize; x++) {
+            vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+        }
     }
     return;
 }
