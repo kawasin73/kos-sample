ec6118782650eb36797e56f5a42dbff22c7c3da3
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-02 12:30:10 +0900

    Chapter 7 : Improve KEYBUF queue

--- int.c
+++ int.c
@@ -35,9 +35,13 @@ void inthandler21(int *esp) {
 
     io_out8(PIC0_OCW2, 0x61); /* IRQ-01 受付完了を PIC に通知 */
     data = io_in8(PORT_KEYDAT);
-    if (keybuf.next < 32) {
-        keybuf.data[keybuf.next] = data;
-        keybuf.next++;
+    if (keybuf.len < 32) {
+        keybuf.data[keybuf.next_w] = data;
+        keybuf.len++;
+        keybuf.next_w++;
+        if (keybuf.next_w == 32) {
+            keybuf.next_w = 0;
+        }
     }
 
     return;
