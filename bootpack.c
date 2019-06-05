0bda3b62b75d1fae8ea0d4ccf1afc1dda52e4792
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-05 23:27:53 +0900

    Chapter 8 : Parse mouse status

--- bootpack.c
+++ bootpack.c
@@ -3,6 +3,7 @@
 
 struct MOUSE_DEC {
     unsigned char buf[3], phase;
+    int x, y, btn;
 };
 
 extern struct FIFO8 keyfifo;
@@ -60,8 +61,17 @@ void HariMain(void)
                 io_sti();
                 if (mouse_decode(&mdec, i) != 0) {
                     /* データが 3 バイト揃ったので表示 */
-                    sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32+8*8-1, 31);
+                    sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+                    if ((mdec.btn & 0x01) != 0) {
+                        s[1] = 'L';
+                    }
+                    if ((mdec.btn & 0x02) != 0) {
+                        s[3] = 'R';
+                    }
+                    if ((mdec.btn & 0x04) != 0) {
+                        s[2] = 'C';
+                    }
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32+15*8-1, 31);
                     putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
                 }
             }
@@ -118,8 +128,12 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat) {
         return 0;
     } else if (mdec->phase == 1) {
         /* マウスの 1 バイト目を待っている段階 */
-        mdec->buf[0] = dat;
-        mdec->phase = 2;
+        if ((dat & 0xc8) == 0x08) {
+            /* 正しい 1 バイト目だった。これによってマウスからの信号が一時的に
+            おかしくなっても、復帰できる */
+            mdec->buf[0] = dat;
+            mdec->phase = 2;
+        }
         return 0;
     } else if (mdec->phase == 2) {
         /* マウスの 2 バイト目を待っている段階 */
@@ -130,6 +144,16 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat) {
         /* マウスの 3 バイト目を待っている段階 */
         mdec->buf[2] = dat;
         mdec->phase = 1;
+        mdec->btn = mdec->buf[0] & 0x07;
+        mdec->x = mdec->buf[1];
+        mdec->y = mdec->buf[2];
+        if ((mdec->buf[0] & 0x10) != 0) {
+            mdec->x |= 0xffffff00;
+        }
+        if ((mdec->buf[0] & 0x20) != 0) {
+            mdec->y |= 0xffffff00;
+        }
+        mdec->y = - mdec->y; /* マウスでは y 方向の符号が画面と反対 */
         return 1;
     }
     return -1; /* ここまでくることはないはず */
