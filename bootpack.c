bece0b168de374901afbd72f0721ef6044604171
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-06 17:12:10 +0900

    Chapter 9 : Change memtest_sub to assembly to avoid compiler optimization

--- bootpack.c
+++ bootpack.c
@@ -128,24 +128,3 @@ unsigned int memtest(unsigned int start, unsigned int end) {
 
     return i;
 }
-
-unsigned int memtest_sub(unsigned int start, unsigned int end) {
-    unsigned int i, *p, old, pat0 = 0xaa55aa55, pat1 = 0x55aa55aa;
-    for (i = start; i <= end; i += 0x1000) {
-        p = (unsigned int *) (i + 0xffc); /* 4KB の末尾の 4 バイトを検査する */
-        old = *p; /* いじる前の値を覚えておく */
-        *p = pat0; /* 試しに書いてみる */
-        *p ^= 0xffffffff;  /* そしてそれを反転してみる */
-        if (*p != pat1) { /* 反転結果になったか */
-not_memory:
-            *p = old;
-            break;
-        }
-        *p ^= 0xffffffff; /* もう一度反転してみる */
-        if (*p != pat0) { /* 元に戻ったか */
-            goto not_memory;
-        }
-        *p = old; /* いじった値を元に戻す */
-    }
-    return i;
-}
