2e314a5c5c187a736329d5879fb4c5da9ead8e86
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-16 11:20:31 +0900

    Chapter 12 : Init PIT

---
+++ timer.c
@@ -0,0 +1,19 @@
+/* タイマ関連 */
+
+#include "bootpack.h"
+
+#define PIT_CTRL        0x0043
+#define PIT_CNT0        0x0040
+
+void init_pit(void) {
+    io_out8(PIT_CTRL, 0x34);
+    io_out8(PIT_CNT0, 0x9c);
+    io_out8(PIT_CNT0, 0x2e);
+    return;
+}
+
+void inthandler20(int *esp) {
+    io_out8(PIC0_OCW2, 0x60); /* IRQ-00 受付官僚を PIC に通知 */
+    /* とりあえず何もしない */
+    return;
+}
