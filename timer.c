c54f7db8b9d58fb3b3b16ff110383b6af3283fa3
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-16 22:54:59 +0900

    Chapter 12 : Improve timer to check oldest timer only

--- timer.c
+++ timer.c
@@ -16,6 +16,7 @@ void init_pit(void) {
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
+    timerctl.next = 0xffffffff;
     for (i = 0; i < MAX_TIMER; i++) {
         timerctl.timer[i].flags = 0; /* 未使用 */
     }
@@ -47,6 +48,10 @@ void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data) {
 void timer_settime(struct TIMER *timer, unsigned int timeout) {
     timer->timeout = timeout + timerctl.count;
     timer->flags = TIMER_FLAGS_USING;
+    if (timerctl.next > timer->timeout) {
+        /* 次回の時刻を更新 */
+        timerctl.next = timer->timeout;
+    }
     return;
 }
 
@@ -54,11 +59,21 @@ void inthandler20(int *esp) {
     int i;
     io_out8(PIC0_OCW2, 0x60); /* IRQ-00 受付官僚を PIC に通知 */
     timerctl.count++;
+    if (timerctl.next > timerctl.count) {
+        return; /* まだ次の時刻なっていないので */
+    }
+    timerctl.next = 0xffffffff;
     for (i = 0; i < MAX_TIMER; i++) {
         if (timerctl.timer[i].flags == TIMER_FLAGS_USING) {
             if (timerctl.timer[i].timeout <= timerctl.count) {
+                /* タイムアウト */
                 timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
                 fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
+            } else {
+                /* まだタイムアウトではない */
+                if (timerctl.next > timerctl.timer[i].timeout) {
+                    timerctl.next = timerctl.timer[i].timeout;
+                }
             }
         }
     }
