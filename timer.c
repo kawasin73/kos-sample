557b0ab3bb02efd9c2f361aa191635ed1fdeb039
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-19 18:03:30 +0900

    Chapter 13 : Improve timer list by setting last timer

--- timer.c
+++ timer.c
@@ -12,15 +12,20 @@ struct TIMERCTL timerctl;
 
 void init_pit(void) {
     int i;
+    struct TIMER *t;
     io_out8(PIT_CTRL, 0x34);
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
-    timerctl.next = 0xffffffff;
-    timerctl.using = 0;
     for (i = 0; i < MAX_TIMER; i++) {
         timerctl.timers0[i].flags = 0; /* 未使用 */
     }
+    t = timer_alloc();
+    t->timeout = 0xffffffff;
+    t->next = 0;
+    t->flags = TIMER_FLAGS_USING;
+    timerctl.t0 = t;
+    timerctl.next = 0xffffffff;
     return;
 }
 
@@ -56,16 +61,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout) {
     e = io_load_eflags();
     io_cli();
 
-    timerctl.using++;
-    if (timerctl.using == 1) {
-        /* 動作中のタイマはこれ１つになる場合 */
-        timerctl.t0 = timer;
-        timer->next = 0;
-        timerctl.next = timer->timeout;
-        io_store_eflags(e);
-        return;
-    }
-
     t = timerctl.t0;
     if (timer->timeout <= t->timeout) {
         /* 先頭に入れる場合 */
@@ -80,9 +75,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout) {
     for (;;) {
         s = t;
         t = t->next;
-        if (t == 0) {
-            break; /* 一番後ろになった */
-        }
         if (timer->timeout <= t->timeout) {
             /* s と t の間に入れる場合 */
             s->next = timer;
@@ -91,16 +83,10 @@ void timer_settime(struct TIMER *timer, unsigned int timeout) {
             return;
         }
     }
-
-    /* 一番後ろに入れる場合 */
-    s->next = timer;
-    timer->next = 0;
-    io_store_eflags(e);
     return;
 }
 
 void inthandler20(int *esp) {
-    int i, j;
     struct TIMER *timer;
     io_out8(PIC0_OCW2, 0x60); /* IRQ-00 受付官僚を PIC に通知 */
     timerctl.count++;
@@ -109,7 +95,7 @@ void inthandler20(int *esp) {
     }
 
     timer = timerctl.t0;
-    for (i = 0; i < timerctl.using; i++) {
+    for (;;) {
         /* timers のタイマは全て動作中のものなので確認しない */
         if (timer->timeout > timerctl.count) {
             break;
@@ -120,13 +106,7 @@ void inthandler20(int *esp) {
         timer = timer->next;
     }
 
-    /* ちょうど i 個のタイマがタイムアウトした。残りをずらす */
-    timerctl.using -= i;
     timerctl.t0 = timer;
-    if (timerctl.using > 0) {
-        timerctl.next = timerctl.t0->timeout;
-    } else {
-        timerctl.next = 0xffffffff;
-    }
+    timerctl.next = timerctl.t0->timeout;
     return;
 }
