ca9f29cbc57616de771977e41a8a2fa91eb7a18d
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-19 18:01:48 +0900

    Fix : mouse fifo bug

--- mouse.c
+++ mouse.c
@@ -12,7 +12,7 @@ void inthandler2c(int *esp) {
     io_out8(PIC1_OCW2, 0x64); /* IRQ-12 受付完了を PIC1 に通知 */
     io_out8(PIC0_OCW2, 0x62); /* IRQ-02 受付完了を PIC0 に通知 */
     data = io_in8(PORT_KEYDAT);
-    fifo32_put(&mousefifo, data + mousedata0);
+    fifo32_put(mousefifo, data + mousedata0);
     return;
 }
 
