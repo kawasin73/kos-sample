07b0033bd7d7c6b599563a3bd6ae45897acecfdf
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-22 18:38:14 +0900

    Chapter 15 : Pass sht_back to task_b via stack

--- bootpack.c
+++ bootpack.c
@@ -12,7 +12,7 @@ struct TSS32 {
     int ldtr, iomap;
 };
 
-void task_b_main(void);
+void task_b_main(struct SHEET * sht_back);
 
 void HariMain(void)
 {
@@ -105,7 +105,7 @@ void HariMain(void)
     set_segmdesc(gdt + 3, 103, (int) &tss_a, AR_TSS32);
     set_segmdesc(gdt + 4, 103, (int) &tss_b, AR_TSS32);
     load_tr(3*8);
-    task_b_esp = memman_alloc_4k(memman, 64*1024) + 64 * 1024;
+    task_b_esp = memman_alloc_4k(memman, 64*1024) + 64 * 1024 - 8;
     tss_b.eip = (int) &task_b_main;
     tss_b.eflags = 0x00000202; /* IF = 1; */
     tss_b.eax = 0;
@@ -123,7 +123,7 @@ void HariMain(void)
     tss_b.fs = 1 * 8;
     tss_b.gs = 1 * 8;
 
-    *((int *) 0x0fec) = (int) sht_back;
+    *((int *) (task_b_esp + 4)) = (int) sht_back;
 
 
     for (;;) {
@@ -268,6 +268,7 @@ void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, i
     boxfill8(sht->buf, sht->bxsize, b, x, y, x+l*8-1, y+15);
     putfonts8_asc(sht->buf, sht->bxsize, x, y, c, s);
     sheet_refresh(sht, x, y, x+l*8, y+16);
+    return;
 }
 
 void make_testbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
@@ -284,23 +285,22 @@ void make_testbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
     return;
 }
 
-void task_b_main(void) {
+void task_b_main(struct SHEET *sht_back) {
     struct FIFO32 fifo;
-    struct TIMER *timer_ts;
+    struct TIMER *timer_ts, *timer_put;
     int i, fifobuf[128], count = 0;
-    char s[11];
-    struct SHEET *sht_back;
+    char s[12];
 
     fifo32_init(&fifo, 128, fifobuf);
     timer_ts = timer_alloc();
-    timer_init(timer_ts, &fifo, 1);
+    timer_init(timer_ts, &fifo, 2);
     timer_settime(timer_ts, 2);
-    sht_back = (struct SHEET *) *((int *) 0x0fec);
+    timer_put = timer_alloc();
+    timer_init(timer_put, &fifo, 1);
+    timer_settime(timer_put, 1);
 
     for (;;) {
         count++;
-        sprintf(s, "%10d", count);
-        putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
 
         io_cli();
         if (fifo32_status(&fifo) == 0) {
@@ -308,7 +308,11 @@ void task_b_main(void) {
         } else {
             i = fifo32_get(&fifo);
             io_sti();
-            if (i == 1) { /* タスクスイッチ */
+            if (i == 1) {
+                sprintf(s, "%11d", count);
+                putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 11);
+                timer_settime(timer_put, 1);
+            } else if (i == 2) {
                 farjmp(0, 3*8);
                 timer_settime(timer_ts, 2);
             }
