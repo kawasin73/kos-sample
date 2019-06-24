ebc80d11c3a385c380bc0b01c26aab750bc08ed1
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-24 21:15:51 +0900

    Chapter 16 : Add TASKCTL

--- timer.c
+++ timer.c
@@ -104,7 +104,7 @@ void inthandler20(int *esp) {
         }
         /* タイムアウト */
         timer->flags = TIMER_FLAGS_ALLOC;
-        if (timer != mt_timer) {
+        if (timer != task_timer) {
             fifo32_put(timer->fifo, timer->data);
         } else {
             ts = 1; /* mt_timer がタイムアウトした */
@@ -115,7 +115,7 @@ void inthandler20(int *esp) {
     timerctl.t0 = timer;
     timerctl.next = timerctl.t0->timeout;
     if (ts != 0) {
-        mt_taskswitch();
+        task_switch();
     }
     return;
 }
