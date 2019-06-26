d31a35dd29366779d79e8f1583e8b89f5b11520b
Kawamura Shintaro <kawasin73@gmail.com>
2019-06-26 23:36:11 +0900

    Chapter 18 : Add cls command

--- bootpack.c
+++ bootpack.c
@@ -1,5 +1,6 @@
 #include "bootpack.h"
 #include <stdio.h>
+#include <string.h>
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
@@ -398,6 +399,7 @@ void console_task(struct SHEET *sheet, int memtotal) {
     int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
     char s[30], cmdline[30];
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+    int x, y;
 
     fifo32_init(&task->fifo, 128, fifobuf, task);
     timer = timer_alloc();
@@ -448,7 +450,7 @@ void console_task(struct SHEET *sheet, int memtotal) {
                     cmdline[cursor_x/8-2] = 0;
                     cursor_y = cons_newline(cursor_y, sheet);
                     /* コマンド実行 */
-                    if (cmdline[0] == 'm' && cmdline[1] == 'e' && cmdline[2] == 'm' && cmdline[3] == 0) {
+                    if (strcmp(cmdline, "mem") == 0) {
                         /* mem コマンド */
                         sprintf(s, "total %dMB", memtotal / (1024 * 1024));
                         putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
@@ -457,6 +459,15 @@ void console_task(struct SHEET *sheet, int memtotal) {
                         putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
                         cursor_y = cons_newline(cursor_y, sheet);
                         cursor_y = cons_newline(cursor_y, sheet);
+                    } else if (strcmp(cmdline, "cls") == 0) {
+                        /* cls コマンド */
+                        for (y = 28; y < 28 + 128; y++) {
+                            for (x = 8; x < 8 + 240; x++) {
+                                sheet->buf[x + y*sheet->bxsize] = COL8_000000;
+                            }
+                        }
+                        sheet_refresh(sheet, 8, 28, 8+240, 28+128);
+                        cursor_y = 28;
                     } else if (cmdline[0] != 0) {
                         putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
                         cursor_y = cons_newline(cursor_y, sheet);
