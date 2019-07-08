4015b61d141989e858890a75fb638ab395f43f4d
Kawamura Shintaro <kawasin73@gmail.com>
2019-07-08 21:17:39 +0900

    Chapter 26 : Remove console limit

--- bootpack.c
+++ bootpack.c
@@ -20,7 +20,7 @@ void HariMain(void)
     struct MOUSE_DEC mdec;
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
     unsigned char *buf_back, buf_mouse[256];
-    struct SHEET *sht_back, *sht_mouse, *sht_cons[2];
+    struct SHEET *sht_back, *sht_mouse;
     struct TASK *task_a, *task;
     static char keytable0[0x80] = {
           0,   0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^',0x08,   0,
@@ -77,8 +77,7 @@ void HariMain(void)
     init_screen8(buf_back, binfo->scrnx, binfo->scrny);
 
     /* sht_cons */
-    sht_cons[0] = open_console(shtctl, memtotal);
-    sht_cons[1] = 0;
+    key_win = open_console(shtctl, memtotal);
 
     /* sht_mouse */
     sht_mouse = sheet_alloc(shtctl);
@@ -89,12 +88,11 @@ void HariMain(void)
     my = (binfo->scrny - 28 - 16) / 2;
 
     sheet_slide(sht_back, 0, 0);
-    sheet_slide(sht_cons[0], 32, 4);
+    sheet_slide(key_win, 32, 4);
     sheet_slide(sht_mouse, mx, my);
     sheet_updown(sht_back, 0);
-    sheet_updown(sht_cons[0], 1);
+    sheet_updown(key_win, 1);
     sheet_updown(sht_mouse, 2);
-    key_win = sht_cons[0];
     keywin_on(key_win);
 
     // sprintf(s, "(%3d, %3d)", mx, my);
@@ -204,13 +202,12 @@ void HariMain(void)
                         io_sti();
                     }
                 }
-                if (i == 256 + 0x3c && key_shift != 0 && sht_cons[1] == 0) { /* Shift + F2 */
-                    sht_cons[1] = open_console(shtctl, memtotal);
-                    sheet_slide(sht_cons[1], 32, 4);
-                    sheet_updown(sht_cons[1], shtctl->top);
+                if (i == 256 + 0x3c && key_shift != 0) { /* Shift + F2 */
                     /* 新しく作ったコンソールを入力状態にする */
                     keywin_off(key_win);
-                    key_win = sht_cons[1];
+                    key_win = open_console(shtctl, memtotal);
+                    sheet_slide(key_win, 32, 4);
+                    sheet_updown(key_win, shtctl->top);
                     keywin_on(key_win);
                 }
                 if (i == 256 + 0x57 && shtctl->top > 2) { /* F11 */
