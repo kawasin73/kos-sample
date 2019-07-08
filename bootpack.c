aa7202b0f785dc1712a0306ba1b7ecc84c475861
Kawamura Shintaro <kawasin73@gmail.com>
2019-07-08 19:14:43 +0900

    Chapter 26 : Optimize sheet (step2)
    update sheet buffer 4 bytes at once

--- bootpack.c
+++ bootpack.c
@@ -42,7 +42,7 @@ void HariMain(void)
           0,   0,   0, '_',   0,   0,   0,   0,   0,   0,   0,   0,   0, '|',   0,   0
     };
     int key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
-    int j, x, y, mmx = -1, mmy = -1;
+    int j, x, y, mmx = -1, mmy = -1, mmx2 = 0;
     struct SHEET *sht = 0, *key_win;
 
     init_gdtidt();
@@ -279,6 +279,7 @@ void HariMain(void)
                                         if (3 <= x && x < sht->bxsize -3 && 3 <= y && y < 21) {
                                             mmx = mx; /* ウィンドウ移動モードへ */
                                             mmy = my;
+                                            mmx2 = sht->vx0;
                                         }
                                         if (sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19) {
                                             /* 「x」ボタンクリック */
@@ -299,8 +300,7 @@ void HariMain(void)
                             /* ウィンドウ移動モード */
                             x = mx - mmx; /* マウスの移動量を計算 */
                             y = my - mmy;
-                            sheet_slide(sht, sht->vx0 + x, sht->vy0 + y);
-                            mmx = mx;
+                            sheet_slide(sht, (mmx2 + x + 2) & ~3, sht->vy0 + y);
                             mmy = my;
                         }
                     } else {
