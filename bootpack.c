8ad6275b02e3e6300f61c85b60ce2cff0ac953bb
Kawamura Shintaro <kawasin73@gmail.com>
2019-07-10 20:16:55 +0900

    Chapter 27 : Shutdown ncst application

--- bootpack.c
+++ bootpack.c
@@ -206,6 +206,7 @@ void HariMain(void)
                         task->tss.eax = (int) &(task->tss.esp0);
                         task->tss.eip = (int) &asm_end_app;
                         io_sti();
+                        task_run(task, -1, 0); /* 終了処理をやらせるために寝ていたら起こす */
                     }
                 }
                 if (i == 256 + 0x3c && key_shift != 0) { /* Shift + F2 */
@@ -295,6 +296,7 @@ void HariMain(void)
                                                 task->tss.eax = (int) &(task->tss.esp0);
                                                 task->tss.eip = (int) &asm_end_app;
                                                 io_sti();
+                                                task_run(task, -1, 0); /* 終了処理をやらせるために寝ていたら起こす */
                                             } else { /* コンソール */
                                                 task = sht->task;
                                                 io_cli();
